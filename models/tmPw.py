287ede1e276bba243d4d5a14baff427d11265091
Victor Ananyev <vindex10@gmail.com>
2017-07-27 11:25:38 +0300

    added model: toy model - plane wave

---
+++ models/tmPw.py
@@ -0,0 +1,36 @@
+import scipy as sp
+from scipy import special
+from multiprocessing.dummy import Pool as ThPool
+from cubature import cubature
+
+from .basic import g, m, mu, eps, INF
+from .basic import mom, beta, eta, coAngle
+
+from ..config import config
+
+# Matrix elements
+def MX(r, q, Crq):
+    return sp.exp(-m*r)/4/sp.pi/r*(sp.exp(1j*r*q*Crq) + sp.exp(-1j*r*q*Crq))
+
+def MP(q, p, Cqp):
+    return 1/(q**2 + p**2 - 2*p*q*Cqp + m**2) + 1/(q**2 + p**2 + 2*q*p*Cqp + m**2)
+
+def sigma_f(x_args, p):
+    """
+        x_args:
+            px[0] - Cpq
+        p:
+            s, MP
+    """
+    px = x_args.T
+
+    return (sp.absolute(p["MP"](mom(p["s"]), mom(p["s"], m), px[0]))**2).T
+
+def sigma(p):
+    """
+        p:
+            s, MP
+    """
+
+    res, err = cubature(sigma_f, 1, 1, [-1], [1], args=[p], abserr=config["abs_err"], vectorized=True)
+    return beta(p["s"])/32/sp.pi/p["s"]*res
