b70fa8a9544e10f7d574b55cda39521e2ac2c7c1
Victor Ananyev <vindex10@gmail.com>
2017-09-12 21:00:20 +0300

    added discrete evaluators

--- analytics.py
+++ analytics.py
@@ -1,5 +1,6 @@
 import scipy as sp
 from scipy import special
+from scipy import misc
 
 from . import constants as CONST
 
@@ -20,22 +21,54 @@ def coAngle(Cpq, Cpr, Fqr):
     return Cpq*Cpr + sp.sqrt(1 - Cpq**2)*sp.sqrt(1 - Cpr**2)*sp.cos(Fqr)
 
 # Matrix elements
-def tmMX(r, q, Crq):
-    return sp.exp(-CONST["m"]*r)/4/sp.pi/r*(sp.exp(1j*r*q*Crq) + sp.exp(-1j*r*q*Crq))
+def tmMX(r, q, Trq):
+    return sp.exp(-CONST["m"]*r)/4/sp.pi/r\
+            *(sp.exp(1j*r*q*sp.cos(Trq)) + sp.exp(-1j*r*q*sp.cos(Trq)))
 
-def tmMP(q, p, Cqp, Fpq):
-    return 1/(q**2 + p**2 - 2*p*q*Cqp + CONST["m"]**2) + 1/(q**2 + p**2 + 2*q*p*Cqp + CONST["m"]**2)
+def tmMP(q, p, Tqp, Fpq):
+    return 1/(q**2 + p**2 - 2*p*q*sp.cos(Tqp) + CONST["m"]**2)\
+                +\
+           1/(q**2 + p**2 + 2*q*p*sp.cos(Tqp) + CONST["m"]**2)
 
-def sqedMP0(p, q, Cpq, Fpq):
-    return 2j*CONST["e"]**2*(1 - p**2*(1-Cpq**2)*(1/(p**2 + q**2 - 2*p*q*Cpq + CONST["m"]**2) + 1/(p**2 + q**2 + 2*p*q*Cpq + CONST["m"]**2)))
+def sqedMP0(p, q, Tpq, Fpq):
+    return 2j*CONST["e"]**2*(1 - p**2*sp.sin(Tpq)**2\
+            *(\
+                1/(p**2 + q**2 - 2*p*q*sp.cos(Tpq) + CONST["m"]**2)\
+                    +\
+                1/(p**2 + q**2 + 2*p*q*sp.cos(Tpq) + CONST["m"]**2)\
+             ))
 
-def sqedMP2(p, q, Cpq, Fpq):
-    return 2j*CONST["e"]**2*p**2*(1-Cpq**2)*(1/(p**2 + q**2 - 2*p*q*Cpq + CONST["m"]**2) + 1/(p**2 + q**2 + 2*p*q*Cpq + CONST["m"]**2))*sp.exp(2j*Fpq)
+def sqedMP2(p, q, Tpq, Fpq):
+    return 2j*CONST["e"]**2*p**2*sp.sin(Tpq)**2\
+            *(\
+                1/(p**2 + q**2 - 2*p*q*sp.cos(Tpq) + CONST["m"]**2)\
+                    +\
+                1/(p**2 + q**2 + 2*p*q*sp.cos(Tpq) + CONST["m"]**2))*sp.exp(2j*Fpq\
+            )
 
 # Wave functions
-def psiColP(k, p, Ckp):
+def psiColP(k, p, Tkp):
     "\psi^{col}_p(k, p, Ckp) - Coulomb wave in momentum space"
-    return -4*sp.pi*sp.exp(-sp.pi*eta(k)/2)*sp.special.gamma(1 + 1j*eta(k))*(
-            2*(p**2 - (k + 1j*CONST["eps"])**2)**(1j*eta(k))*CONST["eps"]*(-1 - 1j*eta(k))/(p**2 + k**2 - 2*p*k*Ckp + CONST["eps"]**2)**(2 + 1j*eta(k)) +
-            2*(k +  1j*CONST["eps"])*eta(k)*(p**2 - (k + 1j*CONST["eps"])**2)**(-1 + 1j*eta(k))/(p**2 + k**2 - 2*p*k*Ckp + CONST["eps"]**2)**(1 + 1j*eta(k))
+    return -4*sp.pi*sp.exp(-sp.pi*eta(k)/2)*sp.special.gamma(1 + 1j*eta(k))\
+            *(\
+                2*(p**2 - (k + 1j*CONST["eps"])**2)**(1j*eta(k))*CONST["eps"]\
+                *(-1 - 1j*eta(k))/(p**2 + k**2 - 2*p*k*sp.cos(Tkp) + CONST["eps"]**2)**(2 + 1j*eta(k))\
+                    +\
+                2*(k +  1j*CONST["eps"])*eta(k)*(p**2 - (k + 1j*CONST["eps"])**2)**(-1 + 1j*eta(k))\
+                /(p**2 + k**2 - 2*p*k*sp.cos(Tkp) + CONST["eps"]**2)**(1 + 1j*eta(k))\
             )
+
+def psiColPdisc(n, l, M, p, Tpq, Fpq):
+    return (-1j)**l *(4*sp.pi) *2**(2*(l+1)) *sp.misc.factorial(l)/(n+l)**2\
+            *sp.sqrt(sp.special.factorial(n-1)/sp.special.factorial(n+2*l))\
+            *(2/CONST["m"]/CONST["g"])**(3/2)*((p*(2/CONST["m"]/CONST["g"]))/(n+l))**l\
+            /(((2/CONST["m"]/CONST["g"])*p)**2 + 1/(n+l)**2)**(2+l)\
+            *sp.special.eval_gegenbauer(n-1\
+                                      , l+1\
+                                      , ((n+l)**2*(p*(2/CONST["m"]/CONST["g"]))**2 - 1)\
+                                        /((n+l)**2*(p*(2/CONST["m"]/CONST["g"]))**2 + 1))\
+            *sp.special.sph_harm(M, l, Fpq, Tpq)
+
+# Spectra
+def energColDisc(n, l):
+    return 2*CONST["m"] - CONST["m"]*CONST["g"]**2/4/(n+l)**2
--- evaluators.py
+++ evaluators.py
@@ -6,7 +6,7 @@ import scipy as sp
 from scipy import special
 from cubature import cubature
 
-from .parallel import npMap
+from .parallel import npMap, pyMap
 from .analytics import mom, energ, beta, eta, coAngle
 
 from . import constants
@@ -46,23 +46,118 @@ class McolPEvaluator:
                 setattr(self, key, val)
         return True
 
-    def compute(self, p, q, Cpq, Fpq):
-        res, err = cubature(self.McolP_f, 3, 2, [0, -1, 0], [self.maxP, 1, 2*sp.pi], args=[p, q, Cpq], abserr=self.absErr, relerr=self.relErr, vectorized=self.vectorized)
+    def compute(self, p, q, Tpq, Fpq):
+        res, err = cubature(self.McolP_f, 3, 2, [0, 0, 0], [self.maxP, sp.pi, 2*sp.pi], args=[p, q, Tpq], abserr=self.absErr, relerr=self.relErr, vectorized=self.vectorized)
         return res[0] + 1j*res[1]
 
-    def McolP_f(self, x_args, p, q, Cpq):
+    def McolP_f(self, x_args, p, q, Tpq):
         """
             x_args:
                 px[0] for px
-                px[1] for Cos(px,p)
+                px[1] for theta(px,p)
                 px[2] for phi
         """
-        res = self.mapper(lambda px: px[0]**2/(2*sp.pi)**3*(energ(p, self.CONST["m"])/energ(px[0], self.CONST["m"]))*sp.conj(self.psiColP(p, px[0], coAngle(Cpq, px[1], px[2])))*self.MP(px[0], q, px[1], px[2]), x_args)
+        res = self.mapper(lambda px:\
+                    sp.sin(px[1])\
+                    *px[0]**2/(2*sp.pi)**3*(energ(p, self.CONST["m"])/energ(px[0], self.CONST["m"]))\
+                    *sp.conj(self.psiColP(p, px[0], coAngle(sp.cos(Tpq), sp.cos(px[1]), px[2])))\
+                    *self.MP(px[0], q, px[1], px[2])\
+                , x_args)
         res = sp.array((sp.real(res), sp.imag(res))).T
 
         if self.monitor is not None:
             self.monitor.push(stackArgRes(x_args, res))
         
+        return res
+
+class McolPDiscEvaluator:
+    def __init__(self, mp, psicolp, denerg):
+        self.CONST = constants
+        self.psiColP = psicolp
+        self.denerg = denerg
+        self.MP = mp
+        
+        self.mapper = npMap
+        self.vectorized = False
+        self.monitor = None
+
+        self.maxP = 1000
+        # be careful with precision, I spent some time to find out its importans
+        self.absErr = 1e-10
+        self.relErr = 1e-8
+
+    def params(self, paramdict=None):
+        keylist = ("maxP"
+                  ,"absErr"
+                  ,"relErr")
+
+        if paramdict is None:
+            return { k: getattr(self, k) for k in keylist }
+
+        for key, val in paramdict.items():
+            if key in keylist:
+                setattr(self, key, val)
+        return True
+
+    def compute(self, n, l):
+        res, err = cubature(self.McolP_f, 3, 2, [0, 0, 0], [self.maxP, sp.pi, 2*sp.pi], args=[n, l], abserr=self.absErr, relerr=self.relErr, vectorized=self.vectorized)
+        return res[0] + 1j*res[1]
+
+    def McolP_f(self, x_args, n, l):
+        """
+            x_args:
+                px[0] for px
+                px[1] for theta(px,p)
+                px[2] for phi
+        """
+        res = self.mapper(lambda px:\
+                    sp.sin(px[1])\
+                    *px[0]**2/(2*sp.pi)**3\
+                    *sp.sqrt(self.denerg(n, l)/2)/energ(px[0], self.CONST["m"])\
+                    *self.psiColP(n, l\
+                                , px[0], px[1], px[2])\
+                    *sp.conj(\
+                        self.MP(px[0]\
+                              , mom(self.denerg(n, l)**2)\
+                              , px[1], px[2])\
+                    )\
+                , x_args)
+        res = sp.array((sp.real(res), sp.imag(res))).T
+
+        if self.monitor is not None:
+            self.monitor.push(stackArgRes(x_args, res))
+        
+        return res
+
+class GammaEvaluator:
+    def __init__(self, mp):
+        self.CONST = constants
+        self.MPEvaluatorInstance = mp
+        self.denerg = self.MPEvaluatorInstance.denerg
+        self.monitor = None
+        self.absErr = 1e-5
+        self.relErr = 1e-3
+
+    def params(self, paramdict=None):
+        keylist = ("absErr"
+                  ,"relErr")
+
+        if paramdict is None:
+            return { k: getattr(self, k) for k in keylist }
+
+        for key, val in paramdict.items():
+            if key in keylist:
+                setattr(self, key, val)
+        return True
+
+    def compute(self, n, l):
+        res = self.CONST["dimfactor"]*self.CONST["Nc"]/(2*l+1)\
+                /(16*sp.pi*self.denerg(n, l))\
+                *sp.absolute(self.MPEvaluatorInstance.compute(n, l))**2
+
+        if self.monitor is not None:
+            self.monitor.push(stackArgRes(sp.array((n , l)), res))
+
         return res
 
 class SigmaEvaluator:
@@ -88,17 +183,26 @@ class SigmaEvaluator:
         return True
 
     def compute(self, s):
-        res, err = cubature(self.sigma_f, 2, 1, [-1, 0], [1, 2*sp.pi], args=[s], abserr=self.absErr, relerr=self.relErr, vectorized=self.vectorized)
+        res, err = cubature(self.sigma_f, 2, 1, [0, 0], [sp.pi, 2*sp.pi], args=[s], abserr=self.absErr, relerr=self.relErr, vectorized=self.vectorized)
         return res[0]
 
     def sigma_f(self, x_args, s):
         """
             x_args:
-                x_args[0] - Cpq
+                x_args[0] - Tpq
                 x_args[1] - Fpq
         """
         # use dimfactor for absErr to be reasonable.
-        res = self.mapper(lambda px: self.CONST["dimfactor"]*self.CONST["Nc"]*beta(s)/64/sp.pi**2/s*sp.absolute(self.MPEvaluatorInstance.compute(mom(s, self.CONST["m"]), mom(s), px[0], px[1]))**2, x_args)
+        res = self.mapper(lambda px:\
+                sp.sin(px[0])\
+                *self.CONST["dimfactor"]*self.CONST["Nc"]*beta(s)/64/sp.pi**2/s\
+                *sp.absolute(\
+                             self.MPEvaluatorInstance.compute(mom(s, self.CONST["m"])\
+                                                            , mom(s)\
+                                                            , px[0]\
+                                                            , px[1])\
+                            )**2\
+                , x_args)
 
         if self.monitor is not None:
             self.monitor.push(stackArgRes(x_args, res))
@@ -147,6 +251,41 @@ class SumruleEvaluator:
             self.monitor.push(stackArgRes(x_args, res))
 
         return res
+
+class SumruleDiscEvaluator:
+    def __init__(self, gamma):
+        self.CONST = constants
+        self.GammaEvaluatorInstance = gamma
+        self.denerg = self.GammaEvaluatorInstance.denerg
+        self.mapper = pyMap
+        self.vectorized = False
+        self.monitor = None
+        self.nMax = 1
+
+    def params(self, paramdict=None):
+        keylist = ("nMax",)
+
+        if paramdict is None:
+            return { k: getattr(self, k) for k in keylist }
+
+        for key, val in paramdict.items():
+            if key in keylist:
+                setattr(self, key, val)
+        return True
+
+    def compute(self):
+        res = sum(self.mapper(\
+                      lambda qn: 16*sp.pi**2*(2*qn[1]+1)\
+                                /self.denerg(*qn)**3\
+                                *self.GammaEvaluatorInstance.compute(*qn)\
+                    , sp.array( [(n, l)\
+                            for n in range(1, self.nMax+1)\
+                            for l in range(n)
+                      ])
+                ))
+
+        return res
+
 
 class TrivialEvaluator:
     def __init__(self, func):
